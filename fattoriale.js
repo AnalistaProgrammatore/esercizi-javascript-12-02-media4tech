43199f055fae2600cfaad31071ab3614c641fa69
Claudio Palzzi <info@media4tech.com>
2020-02-13 12:34:36 +0100

    straFattoriale

--- fattoriale.js
+++ fattoriale.js
@@ -1,4 +1,23 @@
 /**
 * Scrivere qui il codice della funzione fattoriale(n)
 **/
-//stoca
+/*const readline = require('readline')
+
+let num;
+console.log("inserisci un numero cì");
+
+num = parseInt(readline());*/
+
+var num = 5;
+
+var fatto = straFattoRiale(num);
+console.log(fatto);
+  
+
+function straFattoRiale (x) {
+    if (x <= 1) {
+        return 1;
+    }
+    
+    return x * straFattoRiale(x - 1);
+}
